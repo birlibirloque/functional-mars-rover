edcaa5815bc68b6dc75e789e72cf80c235a1c9b6
birlibirloque <manuel.kstro@gmail.com>
2014-04-05 20:35:03 +0200

    Move mars rover turn right three steps and one left when facing east

--- birlibirloque/functional-mars-rover.py
+++ birlibirloque/functional-mars-rover.py
@@ -19,16 +19,34 @@ def backward(mr):
         'W': createAMarsRoverAt({'x':mr['x']+1,'y':mr['y'],'f':mr['f']}),
         }[mr['f']]
 
+def right(mr):
+    return {
+        'N': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'E'}),
+        'E': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'S'}),
+        'S': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'W'}),
+        'W': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'N'}),
+        }[mr['f']]
+
+def left(mr):
+    return {
+        'N': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'W'}),
+        'E': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'N'}),
+        'S': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'E'}),
+        'W': createAMarsRoverAt({'x':mr['x'],'y':mr['y'],'f':'S'}),
+        }[mr['f']]
+
 def move (marsrover,commads):
 	if len(commads) > 1:
 		newmarsrover = createAMarsRoverAt(move(marsrover,commads[:-1]))
 	else:
 		newmarsrover = createAMarsRoverAt(marsrover)
 
-	if (commads[-1] == 'f'):
-		return(fordward(newmarsrover))
-	else:
-		return(backward(newmarsrover))
+	return {
+		'f': fordward(newmarsrover),
+		'b': backward(newmarsrover),
+		'r': right(newmarsrover),
+		'l': left(newmarsrover),
+	}[commads[-1]]
 
 class TestMarsRover (unittest.TestCase):
 
@@ -85,6 +103,17 @@ class TestMarsRover (unittest.TestCase):
 		self.assertEqual(newmarsrover['x'],2)
 		self.assertEqual(newmarsrover['y'],0)
 		self.assertEqual(newmarsrover['f'],'E')
+
+	def test_MoveMarsRoverTurnRightThreeStepsAndOneLeftFacingEast(self):
+		#Given a mars rover at origin point and facing to north
+		originPointAndFacingToNorth = {'x':0, 'y':0, 'f':'E'}
+		marsrover = createAMarsRoverAt (originPointAndFacingToNorth)
+		#When we move mars rover three steps fordward and one backward
+		newmarsrover = createAMarsRoverAt(move(marsrover,'rrrl'))
+		#Then new mars rover is at two setps fordward to north
+		self.assertEqual(newmarsrover['x'],0)
+		self.assertEqual(newmarsrover['y'],0)
+		self.assertEqual(newmarsrover['f'],'W')
 
 if __name__ == '__main__':
 	unittest.main()
